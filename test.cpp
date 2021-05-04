7c4fefc7346fbfdffe318af9c66f00bebca64892
cheyenneayyoub <cayyo001@ucr.edu>
2021-05-04 02:42:08 -0700

    included ContainTest.hpp

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include "spreadsheet.cpp"
 #include "gtest/gtest.h"
 #include "PrintTest.hpp"
+#include "ContainTest.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
