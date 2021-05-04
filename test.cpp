f9fda73594c764704485551246057388fb10e2fc
github-shwenakak <github-shwenakak@gmail.com>
2021-05-04 06:36:00 -0700

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
-#include "spreadsheet.hpp"
-
+#include "spreadsheet.cpp"
+#include "select.hpp"
 #include "gtest/gtest.h"
+#include "testand.cpp"
+#include "testor.cpp"
 
+using namespace std;
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-}
+};
+
